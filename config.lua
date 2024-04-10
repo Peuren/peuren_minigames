6d2feaaf1615568de4c7d8da55ee14151dbc98c2
Peurost <dukuliukas@gmail.com>
2024-04-10 14:03:41 +0300

    fix: Missing config

--- config.lua
+++ config.lua
@@ -1,1 +1,5 @@
 Config = {}
+
+Config.UIImageSource = "ox_inventory/web/images"--Path of your inventory item images, examples:
+    --QBCore: qb-inventory/html/images
+    --Ox: ox_inventory/web/images
